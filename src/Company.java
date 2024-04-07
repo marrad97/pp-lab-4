1d919b1c827bb292f323cb3c386fe8d72a9a0a7a
martyna.radz <mar.rad97@gmail.com>
2024-04-07 09:10:14 +0200

    zad4

--- src/Company.java
+++ src/Company.java
@@ -2,17 +2,20 @@ import employees.*;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Manager("Jan Kowalski", 3000.0, 0);
         employees[1] = new Worker("Anna Kowalska", 3500.0, "HR");
         employees[2] = new Employee("Zbigniew Kowalska", 4000.0);
         employees[3] = new Employee("Katarzyna Kowalska", 4500.0);
         employees[4] = new Worker("Piotr Kowalski", 5000.0, "HR");
+        employees[5] = new Manager("Robert Kowalski", 3200.0, 0);
+        employees[6] = new Worker("Magdalena Kowalska", 3300.0, "HR");
 
-        System.out.println("Pracownik o indeksie 3: " + employees[3]);
 
-        employees[3].setSalary(5500.0);
+        for (Employee employee : employees) {
+            employee.setSalary(employee.getSalary() + 500);
+        }
 
         int nonManagerCount = 0;
         for (Employee employee : employees) {
@@ -21,12 +24,19 @@ public class Company {
             }
         }
 
-        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
-        employees[0].setSalary(7500.0);
+        for (Employee employee : employees) {
+            if (employee instanceof Manager) {
+                Manager manager = (Manager) employee;
+                manager.setNumberOfSubordinates(nonManagerCount);
+                manager.setSalary(7500.0);
+            }
+        }
+
 
         System.out.println("Wszyscy pracownicy:");
         for (Employee employee : employees) {
-            System.out.println(employee);
+            System.out.println(employee.toString());
         }
     }
+
 }
